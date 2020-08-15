34a5a7666b12a9af7a59292da7b6132893375901
Matthew Messinger <mattmess1221@gmail.com>
2020-08-15 14:41:12 -0400

    Don't re-compress the dics if they weren't changeddics are considered changed when their sha1sum changes

--- dump.sh
+++ dump.sh
@@ -13,15 +13,41 @@
 # fedora + centos 8
 # sudo dnf install aspell*
 
-mkdir -p dics
-rm -rf dics/*
+hashsum() {
+    sha1sum "$@" | cut -d' ' -f1
+}
 
-aspell dump dicts | while read -r lang; do
+mkdir -p dics
+
+aspell dump dicts | cut -d'-' -f1 | sort | uniq | while read -r lang; do
     key="${lang:0:2}"
     mkdir -p "dics/$key"
     dic="dics/$key/$lang.dic"
 
-    echo "Dumping $dic"
+    echo "INFO: Dumping $lang"
 
-    aspell --encoding=UTF-8 --lang="$lang" dump master | tee "$dic" | gzip > "$dic.gz"
+    if [ -f "$dic" ]; then
+        old_hash=$(hashsum "$dic")
+    fi
+
+    aspell --encoding=UTF-8 --lang="$lang" dump master > "$dic"
+    if egrep -iq "/[AMCKLSOZ]" "$dic"; then
+        echo "INFO: Expanding affixes in $dic."
+        aspell --encoding=UTF-8 expand < "$dic" | tr " " "\n" > "$dic.expanded"
+        
+        if grep -q "?" "$dic.expanded"; then
+            echo "WARN: Failed to expand affixes in $dic"
+            rm "$dic.expanded"
+        else
+            mv "$dic.expanded" "$dic"
+        fi
+    fi
+
+    new_hash=$(hashsum "$dic" | tee "$dic.sha1")
+
+    if [ "$old_hash" != "$new_hash" ]; then
+        echo "UPDATE: $lang dictionary had an update! Recompressing."
+        gzip < "$dic" > "$dic.gz" 
+        hashsum "$dic.gz" > "$dic.gz.sha1"
+    fi
 done
